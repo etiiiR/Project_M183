c04db877c466e61a9a2dd10ac314dcb386fe2844
Etienne Roulet <etienne.roulet@gibmit.ch>
2018-11-27 13:52:06 +0100

    sdf

--- Moneyshare/bankaccount/views.py
+++ Moneyshare/bankaccount/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Bankaccoount
 
 # Create your views here.
+from django.views import generic
+
+class BookListView(generic.ListView):
+    model = Bankaccoount
