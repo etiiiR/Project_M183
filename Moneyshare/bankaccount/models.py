177e571f289dd33f49739bf4d378378c8088e385
Etienne Roulet <etienne.roulet@gibmit.ch>
2018-11-27 11:35:05 +0100

    commit model

--- Moneyshare/bankaccount/models.py
+++ Moneyshare/bankaccount/models.py
@@ -1,3 +1,11 @@
-from django.db import models
-
 # Create your models here.
+from django.db import models
+from django.conf import settings
+
+
+class Bankaccoount(models.Model):
+    User = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    Money = models.IntegerField(default=1)
